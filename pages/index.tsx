400a6ce6a53d117b4b8035c25c105d1d841c1d19
Zybren Carbonilla <zycarbonilla@gmail.com>
2023-03-03 22:38:37 +0800

    ICPMaker table and button

--- pages/index.tsx
+++ pages/index.tsx
@@ -59,6 +59,7 @@ const Home: NextPage = () => {
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Generate Blog Post</button>
           </div>
         </div>
+        
         <div>
         <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
             <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
